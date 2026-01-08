7a94598d96f4cb56b98b0a8193259afb4c937368
parsa <mpar91863@gmail.com>
2026-01-08 18:34:19 +0330

    Add files via upload

--- codeito (3).js
+++ codeito (3).js
@@ -1,9 +1,34 @@
+function mosh() {
+    document.querySelectorAll(".psh").forEach(div => {
+    div.style.transfor = "calc(2)";
+    });
+    
+}
+
+
+function ser() {
+    document.querySelectorAll(".kamel").forEach(div => {
+    div.style.display = "block";
+    });
+    
+}
+
+function mto() {
+    document.querySelectorAll(".kamel").forEach(div => {
+    div.style.display = "none";
+    });
+    
+}
 
 function searchDivs() {
     document.querySelector(".border").style.display = "none";
     document.querySelectorAll(".chi").forEach(div => {
     div.style.display = "none";
     });
+    
+
+    
+    
 
     document.querySelector(".sh").style.display = "none";
     
